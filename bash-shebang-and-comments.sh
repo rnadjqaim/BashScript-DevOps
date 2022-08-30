cb1a661c854f3503f2a8ca176af1ce442e207e83
Rnad Abu Jqaim <103530494+rnadjqaim@users.noreply.github.com>
2022-08-30 17:47:34 +0300

    Create bash-shebang-and-comments.sh

---
+++ bash-shebang-and-comments.sh
@@ -0,0 +1,6 @@
+#shebang :   #!/bin/bash, is the directive , the default shell 
+#the shebang indicates the direction of path to the bash shell
+which bash 
+which -a bash 
+ls -li
+# ls -ltr : To sort the file names in the last modification time in reverse order
